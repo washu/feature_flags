188246cd5889162d681e520f9b182d9307cf7f46
Sal Scotto <sal.scotto@gmail.com>
2014-01-10 11:20:39 -0500

    updated generator and model to use mongoid

--- lib/generators/feature_flags/templates/feature_flag_model.rb
+++ lib/generators/feature_flags/templates/feature_flag_model.rb
@@ -1,11 +1,12 @@
 class Feature
   include Mongoid::Document
-
+  include Mongoid::Timestamps
   #### IMPORTANT start ####
   ### Dont remove this as this required for memoization of features...
   include FeatureFlags::FeatureBase
   #### IMPORTANT end ####
-
+  field :name, type: String
+  field :status, type: Boolean
 
   validates :name, presence: true
 
